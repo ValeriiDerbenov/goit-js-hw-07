81e12ba841ff18e3897b21dcdcd3310d167dc079
Valerii <vd.alternative@gmail.com>
2023-10-24 00:02:49 +0300

    1

--- js/task-07.js
+++
@@ -1,9 +0,0 @@
-const controller = document.querySelector("#font-size-control");
-const text = document.querySelector("#text");
-
-controller.addEventListener("input", onFontController);
-
-function onFontController() {
-  const size = controller.value;
-  text.style.fontSize = `${size}px`;
-}
