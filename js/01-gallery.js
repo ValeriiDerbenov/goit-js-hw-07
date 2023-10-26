450a7576b9c8dbbf1fc12b50743f76607469ecaa
Valerii <vd.alternative@gmail.com>
2023-10-26 23:31:08 +0300

    01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const imageMarkup = createImageListMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", imageMarkup);
+
+function createImageListMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+		<li class="gallery__item">
+<a class="gallery__link" href="${original}">
+  <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+  />
+</a>
+</li>
+		`;
+    })
+    .join("");
+}
+
+const links = galleryContainer.querySelectorAll(".gallery__link");
+
+links.forEach((el) => el.addEventListener("click", (e) => e.preventDefault()));
+
+galleryContainer.addEventListener("click", onImageClick);
+
+function onImageClick(event) {
+  if (event.target.nodeName === "IMG") {
+    const instance = basicLightbox.create(
+      `<img src="${event.target.dataset.source}" width="800" height="600">`
+    );
+
+    instance.show();
+  }
+  event.stopPropagation();
+}
